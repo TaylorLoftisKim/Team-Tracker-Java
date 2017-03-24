6c05f3b3c4ca9f984ff7e08cd74679a16cf7bdee
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-24 13:42:31 -0700

    Make further Front End Logic, Currently allows the user to create multiple teams, does not display new teams currently.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,18 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/team-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Team newTeam = new Team(name);
+      model.put("template", "templates/team-created.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
