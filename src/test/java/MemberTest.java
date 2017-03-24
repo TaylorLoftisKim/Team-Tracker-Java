6b972e4dfe5abc2753e5c266b279eec2aec2c5aa
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-24 09:32:06 -0700

    Create Second passing test for Member Class

--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -5,7 +5,13 @@ public class MemberTest {
 
   @Test
   public void Member_instantiatesCorrectly_true() {
-    Member myMember = new Member("Taylor");
+    Member myMember = new Member("");
     assertEquals(true, myMember instanceof Member);
+  }
+
+  @Test
+  public void Member_instantiatesWithName_String() {
+    Member myMember = new Member("Taylor");
+    assertEquals("Taylor", myMember.getName());
   }
 }
