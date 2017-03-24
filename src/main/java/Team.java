cc10162e3c0a949cc048c062a156ac56ac373a87
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-24 10:15:16 -0700

    Passing first test for Team class, build out Team object

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,16 +1,40 @@
-// import java.util.List;
-// import java.util.ArrayList;
-//
-// public class Team {
-//   private String mTeamName;
-//   private static List<Team> instances = new ArrayList<Team>();
-//   private int mId;
-//   private List<Member> mMembers;
-//
-//   public Team(String teamName) {
-//     mTeamName = teamName;
-//     instances.add(this);
-//     mId = instances.add(this);
-//     mMembers = new ArraryList<Member>();
-//   }
-// }
+import java.util.List;
+import java.util.ArrayList;
+
+public class Team {
+  private String mTeamName;
+  private static List<Team> instances = new ArrayList<Team>();
+  private int mId;
+  private List<Member> mMembers;
+
+  public Team(String teamName) {
+    mTeamName = teamName;
+    instances.add(this);
+    mId = instances.size();
+    mMembers = new ArrayList<Member>();
+  }
+
+  public String getTeamName() {
+    return mTeamName;
+  }
+
+  public static List<Team> all() {
+    return instances;
+  }
+
+  public static void clear(){
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Team find(int id) {
+    return instances.get(id -1);
+  }
+
+  public void addMember(Member member) {
+    mMembers.add(member);
+  }
+}
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -1,11 +1,12 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-//
-// public class TeamTest {
-//
-//   @Test
-//   public void newTeam_instantiatesCorrectly() {
-//     Team testTeam = new Team("Coders");
-//     Member
-//   }
-// }
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TeamTest {
+
+  @Test
+  public void newTeam_instantiatesCorrectly() {
+    Team testTeam = new Team("Coders");
+    Member testMember = new Member("Taylor");
+    assertEquals(true, testTeam instanceof Team);
+  }
+}
