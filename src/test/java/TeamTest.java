a1e965660ea57a158be0b61f44ef8f50d6db60e6
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-24 10:20:28 -0700

    create second passing test for Team object

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -5,8 +5,14 @@ public class TeamTest {
 
   @Test
   public void newTeam_instantiatesCorrectly() {
-    Team testTeam = new Team("Coders");
-    Member testMember = new Member("Taylor");
+    Team testTeam = new Team("");
+    Member testMember = new Member("");
     assertEquals(true, testTeam instanceof Team);
+  }
+
+  @Test
+  public void Team_instantiatesWithName_String() {
+    Team testTeam = new Team("Epicoders");
+    assertEquals("Epicoders", testTeam.getTeamName());
   }
 }
