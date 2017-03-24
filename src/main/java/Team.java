7f08feb6f8c8f4b895de2e90e557d60b89bb8e58
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-03-24 08:45:46 -0700

    Initial commit, begin creating classes and variables

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,11 @@
-public class Team {
+import java.util.List;
+import java.utilArrayList;
 
+public class Team {
+  private String mTeamName;
+  private static List<Team> instances = new ArrayList<Team>();
+  private int mId;
+  private List<Member> mMembers;
+
+  public Team(String teamName)
 }
